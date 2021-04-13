d53ea3e008f5a090f260c823e2206640f6cc0fd6
Khinshan Khan <khanshan501@gmail.com>
2021-04-13 15:21:33 -0400

    chose news hub idea

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Home = () => {
-  return <>Home</>;
+  return (
+    <>
+      <h1>News Hub</h1>
+      Take it to the next level!
+    </>
+  );
 };
 
 export default Home;
